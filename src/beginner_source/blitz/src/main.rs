2478c7bfc87b07160d7e47dfb10a879a62f7fa8a
lamnguyenx <lamfm95@gmail.com>
2024-10-05 17:22:46 +0700

    // setup the workspace

---
+++ src/beginner_source/blitz/src/main.rs
@@ -0,0 +1,6 @@
+mod tensor_tutorial;
+
+pub fn main() {
+
+    tensor_tutorial::tensor_tutorial();
+}
--- src/beginner_source/blitz/src/tensor_tutorial.rs
+++ src/beginner_source/blitz/src/tensor_tutorial.rs
@@ -1,6 +1,6 @@
 use candle_core::{Device, Tensor};
 
-fn main() {
+pub fn tensor_tutorial() {
     println!("-----------------------------------------------");
     let x = Tensor::new(&[1.0, 2.0], &Device::Cpu).unwrap();
     dbg!(x);
